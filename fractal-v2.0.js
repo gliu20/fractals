d2bfe164496b938a2f4ca8ea79f6440e82098a1d
George <gliu10000@gmail.com>
2020-07-27 17:02:39 -0400

    add zSquaredPlusC

--- fractal-v2.0.js
+++ fractal-v2.0.js
@@ -5,6 +5,12 @@
 
 const qfm = {}
 
+qfm.zSquaredPlusC = (zComplex,cComplex) => {
+  const zSquared = qfm.complexSquare(zComplex);
+  
+  return qfm.complexAdd(zSquared,cComplex);
+}
+
 qfm.complexSquare = ({real,imag}) => {
   const sum = real + imag;
   const diff = real - imag;
@@ -14,6 +20,16 @@ qfm.complexSquare = ({real,imag}) => {
     real: sum * diff,
     imag: prod + prod
   }
+}
+
+qfm.complexAdd = (complex1,complex2) => {
+  const {real:real1,imag:imag1} = complex1;
+  const {real:real2,imag:imag2} = complex2;
+  
+  return {
+    real: real1 + real2,
+    imag: imag1 + imag2
+  };
 }
 
 qfm.complexMult = (complex1,complex2) => {
