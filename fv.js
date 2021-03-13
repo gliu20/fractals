26ef620ae7111436d902277f906aadcfc7b2dc51
gliu20 <gliu10000@gmail.com>
2021-03-12 22:48:46 -0500

    improve throttle reset

--- fv.js
+++ fv.js
@@ -77,7 +77,7 @@ fv._throttleAt = (fps, callback, infoCallback) => {
 			// exponentially
 			// so it's like AIMD
 			// we also decrease multiplicativly as well
-			mergeInvocate *= 0.8;
+			mergeInvocate *= 0.85;
 			mergeInvocate -= skippedInvocations;
 			mergeInvocate = Math.floor(mergeInvocate);
 
