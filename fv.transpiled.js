1823e0c9dd92f5557009c556ea3007b980a403da
George <24921711+gliu20@users.noreply.github.com>
2021-03-17 13:41:29 -0400

    updated transpile

--- fv.transpiled.js
+++ fv.transpiled.js
@@ -1,15 +1,3 @@
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
-
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
@@ -249,7 +237,7 @@ fv.genLookupTable = /*#__PURE__*/function () {
 
             // only if we didn't subdivide this then we subdivide this
             if (!index[lookup[j].join(",")]) {
-              lookup.push.apply(lookup, _toConsumableArray(fv.genSubdividedLookup.apply(fv, _toConsumableArray(lookup[j]))));
+              lookup.push.apply(lookup, fv.genSubdividedLookup.apply(fv, lookup[j]));
               index[lookup[j].join(",")] = true;
             }
 
@@ -298,9 +286,9 @@ fv.genLookupTable = /*#__PURE__*/function () {
             return fv._wait();
 
           case 28:
-            if (!index.hasOwnProperty("".concat(lookup[i][0], ",").concat(lookup[i][1]))) {
+            if (!index.hasOwnProperty(lookup[i][0] + "," + lookup[i][1])) {
               // mark as done
-              index["".concat(lookup[i][0], ",").concat(lookup[i][1])] = true; // move old lookup to cleanLookup
+              index[lookup[i][0] + "," + lookup[i][1]] = true; // move old lookup to cleanLookup
 
               cleanLookup.push(lookup[i]);
             }
