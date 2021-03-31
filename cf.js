518b379f017e36837e1141459f343c3a030a89f9
George <24921711+gliu20@users.noreply.github.com>
2021-03-31 17:50:14 -0400

    fix blue orange color interpolation

--- cf.js
+++ cf.js
@@ -21,12 +21,32 @@ cf.grayScale = (i,maxIterations, coloring) => {
 }
 
 cf.blueOrange = (i,maxIterations, coloring) => {
-  const adjustedI = i * coloring.colorScale + coloring.colorOffset;
-  const hue = adjustedI % 160;
-  const brightness = 4 * adjustedI ** 2 / 160 ** 2 - 4 * adjustedI / 160 + 1;
-  if (i === maxIterations) { return "#000000"; }
   
-  return "hsl("+(hue + 40)+",100%,"+Math.floor(brightness * 100)+"%)";
+  if (i === maxIterations) { return "#000000"; }
+
+  const adjustedI = i * coloring.colorScale + coloring.colorOffset;
+
+  let colorValue = adjustedI % 400 / 2;
+  
+  // make blue orange circular
+	if (colorValue > 100) {
+    colorValue = 200 - colorValue;
+  }
+
+  const colorStop1 = [6, 12, 102];
+  const colorStop2 = [255, 255, 255];
+  const colorStop3 = [218, 106, 8];
+
+  const interpolate = (val1, val2, extent) => (val1 * (1 - extent) + val2 * extent)
+
+  const r = interpolate(colorStop1[0], colorStop2[0], colorValue * 2 / 100) * (colorValue <= 50) +
+            interpolate(colorStop2[0], colorStop3[0], colorValue * 2 / 100 - 1) * (colorValue > 50);
+  const g = interpolate(colorStop1[1], colorStop2[1], colorValue * 2 / 100) * (colorValue <= 50) +
+            interpolate(colorStop2[1], colorStop3[1], colorValue * 2 / 100 - 1) * (colorValue > 50);
+  const b = interpolate(colorStop1[2], colorStop2[2], colorValue * 2 / 100) * (colorValue <= 50) +
+            interpolate(colorStop2[2], colorStop3[2], colorValue * 2 / 100 - 1) * (colorValue > 50);
+
+  return "rgb("+Math.floor(r)+","+Math.floor(g)+","+Math.floor(b)+")";
 }
 
 cf.rainbow = (i,maxIterations, coloring) => {
