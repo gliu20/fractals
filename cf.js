b1694640e2b2c648472cccc29bab8c1600d684aa
George <24921711+gliu20@users.noreply.github.com>
2021-03-31 16:39:39 -0400

    fix colorOffset bug

--- cf.js
+++ cf.js
@@ -7,21 +7,21 @@ const cf = {};
 
 
 cf.rouge = (i,maxIterations, coloring) => {
-  const adjustedI = i * coloring.colorScale + coloring.offset;
+  const adjustedI = i * coloring.colorScale + coloring.colorOffset;
   if (i === maxIterations) { return "#000000"; }
   
   return "hsl(0,100%,"+(adjustedI % 100)+"%)";
 }
 
 cf.grayScale = (i,maxIterations, coloring) => {
-  const adjustedI = i * coloring.colorScale + coloring.offset;
+  const adjustedI = i * coloring.colorScale + coloring.colorOffset;
   if (i === maxIterations) { return "#000000"; }
   
   return "hsl(0,0%,"+(adjustedI % 100)+"%)";
 }
 
 cf.blueOrange = (i,maxIterations, coloring) => {
-  const adjustedI = i * coloring.colorScale + coloring.offset;
+  const adjustedI = i * coloring.colorScale + coloring.colorOffset;
   const hue = adjustedI % 160;
   const brightness = 4 * adjustedI ** 2 / 160 ** 2 - 4 * adjustedI / 160 + 1;
   if (i === maxIterations) { return "#000000"; }
@@ -30,7 +30,7 @@ cf.blueOrange = (i,maxIterations, coloring) => {
 }
 
 cf.rainbow = (i,maxIterations, coloring) => {
-  const adjustedI = i * coloring.colorScale + coloring.offset;
+  const adjustedI = i * coloring.colorScale + coloring.colorOffset;
   if (i === maxIterations) { return "#000000"; }
   
   return "hsl("+(adjustedI % 360)+",100%,50%)";
