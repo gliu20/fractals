59bc519df487b7b6946bce2104b10a38ae032e61
George <24921711+gliu20@users.noreply.github.com>
2021-03-31 12:01:38 -0400

    fix vb calcCenterAfterZoom to use correct viewbox

--- vb.js
+++ vb.js
@@ -77,10 +77,10 @@ vb.calcHalfSpanAfterZoom = (spanX, spanY, zoomMultiplier) => {
 
 vb.calcCenterAfterZoom = (mouseX, mouseY, canvasX, canvasY, centerX, centerY, zoomedSpanX, zoomedSpanY, viewbox, width, height) => {
   const viewboxWithoutMouseOffset = vb.calcViewbox(centerX, centerY, zoomedSpanX, zoomedSpanY);
-  const { canvasX: zoomedCanvasX, canvasY: zoomedCanvasY } = vb.mouseCoordsToCanvasCoords(mouseX, mouseY, viewbox, width, height);
+  const { canvasX: zoomedCanvasX, canvasY: zoomedCanvasY } = vb.mouseCoordsToCanvasCoords(mouseX, mouseY, viewboxWithoutMouseOffset, width, height);
   
   const zoomedCenterX = centerX + (canvasX - zoomedCanvasX);
-	const zoomedCenterY = centerY + (canvasY - zoomedCanvasY);
+  const zoomedCenterY = centerY + (canvasY - zoomedCanvasY);
   
   return {
     zoomedCenterX,
