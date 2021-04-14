45030591e06137666f0a3015493b8767053df36f
gliu20 <24921711+gliu20@users.noreply.github.com>
2021-04-13 22:17:24 -0400

    prototype fractal math fundamentals

--- c/src/qfm.c
+++ c/src/qfm.c
@@ -1,5 +1,52 @@
 # include <stdio.h>
 
+
+
+void zSquaredPlusC (double zReal, double zImag,
+    double cReal, double cImag,
+    double *real, double *imag) {
+
+    complexSquare(zReal, zImag, real, imag);
+    complexAdd(zReal, zImag, *real, *imag, real, imag);
+}
+
+void complexSquare(double real1, double imag1,
+    double *real, double *imag) {
+
+    const double sum = real1 + imag1;
+    const double diff = real1 - imag1;
+    const double prod = real1 * imag1;
+
+    *real = sum * diff;
+    *imag = prod + prod;
+}
+
+
+void complexAdd (double real1, double imag1, 
+    double real2, double imag2, 
+    double *real, double *imag) {
+
+    *real = real1 + real2;
+    *imag = imag2 + imag2;
+}
+
+void complexMult (double real1, double imag1, 
+    double real2, double imag2, 
+    double *real, double *imag) {
+    
+    const double realProd = real1 * real2;
+    const double imagProd = imag1 * imag2;
+
+    const double sumProd = (real1 + imag1) * (real2 + imag2);
+
+    *real = realProd - imagProd;
+    *imag = sumProd - realProd - imagProd;
+}
+
+int complexHalfNorm (double real, double imag) {
+    return real * real + imag * imag;
+}
+
 int main() {
     printf("hw!\n");
     return 0;
