5967541d15e88143f9a6251081c104948ea1ef93
gliu20 <24921711+gliu20@users.noreply.github.com>
2021-04-15 00:00:43 -0400

    add some test build code

--- c/build.md
+++ c/build.md
@@ -2,9 +2,6 @@
 Run the following
 
 ```sh
-emcc -O3 -flto -s ENVIRONMENT=web,worker src/qfm.c -o dist/index.html
 
-~/emsdk/upstream/bin/wasm-opt -O4 --vacuum -c -ffm dist/index.wasm -o dist/index.min.wasm
-~/emsdk/upstream/bin/wasm-dis dist/index.min.wasm -o dist/index.min.wat
-~/emsdk/upstream/bin/wasm-dis dist/index.wasm -o dist/index.wat
 ```
+
---
+++ c/build.sh
@@ -0,0 +1,74 @@
+
+emcc -O3 -flto -s ENVIRONMENT=web,worker src/qfm.c -o dist/index.html
+~/emsdk/upstream/bin/wasm-opt -O4 -ffm -aimfs 1000 -ifwl \
+--dce \
+--dae-optimizing \
+--directize \
+--inlining-optimizing \
+--licm \
+-lmu \
+--optimize-added-constants \
+--optimize-added-constants-propagate \
+--optimize-instructions \
+--precompute \
+--precompute-propagate \
+--remove-memory \
+--remove-unused-brs \
+--remove-unused-names \
+--remove-unused-module-elements \
+--remove-unused-nonfunction-module-elements \
+--reorder-functions \
+--reorder-locals \
+--simplify-globals \
+--simplify-globals-optimizing \
+--simplify-locals \
+--simplify-locals-notee-nostructure \
+--coalesce-locals \
+--coalesce-locals-learning \
+--merge-blocks \
+--merge-locals \
+--vacuum \
+
+dist/index.wasm -o dist/index.min.wasm
+
+
+~/emsdk/upstream/bin/wasm-dis dist/index.wasm -o dist/index.wat
+~/emsdk/upstream/bin/wasm-dis dist/index.min.wasm -o dist/index.min.wat
+
+
+
+
+# --flatten \
+# --enable-nontrapping-float-to-int \
+# --dae-optimizing \
+# --dce \
+# --dfo \ # this
+# --directize \
+# --inlining-optimizing \
+# --licm \
+# --local-cse \
+# --optimize-added-constants \
+# --optimize-added-constants-propagate \
+# --optimize-instructions \
+# --optimize-stack-ir \
+# --precompute \
+# --precompute-propagate \
+# --remove-memory \
+# --remove-unused-brs \
+# --remove-unused-names \
+# --remove-unused-module-elements \
+# --remove-unused-nonfunction-module-elements \
+# --reorder-functions \
+# --reorder-locals \
+# --rereloop \
+# --rse \
+# --simplify-globals \
+# --simplify-globals-optimizing \
+# --simplify-locals \
+# --simplify-locals-notee-nostructure \
+# --coalesce-locals \
+# --coalesce-locals-learning \
+# --merge-blocks \
+# --merge-locals \
+# --vacuum \
+# dist/index.wasm -o dist/index.min.wasm
