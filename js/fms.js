d85d0fb3c4cb8149b7c05ac02a78c2bb39cfed63
George <24921711+gliu20@users.noreply.github.com>
2021-04-27 13:02:06 -0400

    add shape candidate maker

--- js/fms.js
+++ js/fms.js
@@ -5,7 +5,7 @@
 
 var fms = {};
 
-fms.sum = (...rest) => rest.reduce((a,b) => a + b); 
+fms.sum = (arr) => arr.flat().reduce((a,b) => a + b); 
 
 fms.createDiagonalMatrix = (s,d) => {
   const w = s;
@@ -31,10 +31,10 @@ fms.createMatrix = (w,h) => {
   return arr;
 }
 
-fms.fillRect = (matrix,x,y,w,h) => {
+fms.fillRect = (matrix,x,y,w,h,v) => {
   for (var i = 0; i < h; i++) {
     for (var j = 0; j < w; j++) {
-      matrix[i + y][j + x] = 1;
+      matrix[i + y][j + x] = v || 1;
     }
   }
   return matrix;
@@ -68,13 +68,27 @@ fms.sub = (a, b) => {
   return arr;
 }
 
+fms.mul = (a, b) => {
+  const h = a.length;
+  const w = a[0].length;
+  const arr = [];
+
+  for (var i = 0; i < h; i++) {
+    arr.push([]);
+    for (var j = 0; j < w; j++) {
+      arr[i][j] = a[i][j] * b[i][j];
+    }
+  }
+  return arr;
+}
+
 fms.genTargets = (n) => {
   const targets = [];
   for (let i = 0; i < (2 * n - 1); i++) {
     const diagonal = fms.createDiagonalMatrix(n,i);
     targets.push({
       diagonal: diagonal,
-      targetScalar: fms.sum(...diagonal.flat()),
+      targetScalar: fms.sum(diagonal),
 //      reachedByShapes: {}
     });
   }
@@ -88,8 +102,8 @@ fms.enumerateShapes = (n) => {
       for (let y = 0; y < n; y++) {
         for (let h = n - y; h >= 1; h--) {
           enumeratedShapes.push({
-            shape: [x,y,w,h],
-//            reachesTargets: {}
+            shape: fms.fillRect(fms.createMatrix(n,n),x,y,w,h),
+            history: [{type:"seed",shape:[x,y,w,h]}]
           })
         }
       }
@@ -98,11 +112,81 @@ fms.enumerateShapes = (n) => {
   return enumeratedShapes;
 }
 
-fms.solve = (n) => {
+fms.genFitness = (n) => {
   const targets = fms.genTargets(n);
+  for (let target of targets) {
+    target.fitness = fms.sub(
+      target.diagonal,
+      fms.fillRect(
+        fms.createMatrix(n,n),0,0,n,n
+      )
+    );
+  }
+  return targets;
+}
+
+fms.evalFitnessPartial = (matrix,fitness) => {
+  const exactMatchReward = Math.exp(fms.sum(fms.mul(matrix,fitness)));
+  const meanSquaredError = fms.mul(fms.sub(matrix,fitness),fms.sub(matrix,fitness));
+
+  return Math.exp(exactMatchReward + meanSquaredError);
+}
+
+fms.evalFitness = (matrix,targetFitness) => {
+  let fitness = 0;
+  for (let target of targetFitness) {
+    fitness += Math.floor(fms.evalFitnessPartial(matrix,target.fitness));
+  }
+  return fitness;
+}
+
+fms.genShapeCandidates = (shapeCandidates,iterations) => {
+  const len = shapeCandidates.length;
+  for (let i = 0; i < iterations; i++) {
+    const shape1 = shapeCandidates[Math.floor(Math.random() * len)];
+    const shape2 = shapeCandidates[Math.floor(Math.random() * len)];
+
+    if (Math.floor(Math.random() * 2)) {
+      shapeCandidates.push({
+        shape: fms.add(shape1.shape, shape2.shape),
+        history: [...shape1.history, {type:"add",shape1,shape2}]
+      });
+    }
+    else {
+      shapeCandidates.push({
+        shape: fms.sub(shape1.shape, shape2.shape),
+        history: [...shape1.history, {type:"sub",shape1,shape2}]
+      });
+    }
+  }
+}
+
+fms.scoreCandidates = (shapeCandidates,targetFitness) => {
+  for (let candidate of shapeCandidates) {
+    candidate.fitnessScore = fms.evalFitness(candidate.shape, targetFitness);
+  }
+}
+
+fms.pruneCandidates = (shapeCandidates, threshold) => {
+  for (let i = 0; i < shapeCandidates.length; i++) {
+    const candidate = shapeCandidates[i];
+    if (Math.floor(Math.random() * (candidate.fitnessScore + threshold)) === 0) {
+      // delete shape
+      shapeCandidates.splice(i,1);
+      i--;
+    }
+  }
+}
+
+fms.solve = (n) => {
+  const targetFitness = fms.genFitness(n);
   const enumeratedShapes = fms.enumerateShapes(n);
+  const shapeCandidates = [...enumeratedShapes];
 
-  
+  fms.genShapeCandidates(shapeCandidates,20);
+  console.log({shapeCandidates});
+  fms.scoreCandidates(shapeCandidates,targetFitness);
+  fms.pruneCandidates(shapeCandidates, 10);
 
-  console.log({targets, enumeratedShapes});
+  console.log({targetFitness, shapeCandidates});
 } 
