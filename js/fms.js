8000eb085e0a57b1b169fa00e1fd949980f1af56
George <24921711+gliu20@users.noreply.github.com>
2021-04-27 13:03:16 -0400

    fix fitness partial

--- js/fms.js
+++ js/fms.js
@@ -127,9 +127,9 @@ fms.genFitness = (n) => {
 
 fms.evalFitnessPartial = (matrix,fitness) => {
   const exactMatchReward = Math.exp(fms.sum(fms.mul(matrix,fitness)));
-  const meanSquaredError = fms.mul(fms.sub(matrix,fitness),fms.sub(matrix,fitness));
+  const meanSquaredError = fms.sum(fms.mul(fms.sub(matrix,fitness),fms.sub(matrix,fitness)));
 
-  return Math.exp(exactMatchReward + meanSquaredError);
+  return Math.exp(2 * exactMatchReward + meanSquaredError);
 }
 
 fms.evalFitness = (matrix,targetFitness) => {
